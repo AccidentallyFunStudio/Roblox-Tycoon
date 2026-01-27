ee7b27f39d29499f3a37bf1d42e6d236e72e8352
rizaldysdw <rizaldysdw@gmail.com>
2026-01-28 04:32:20 +0700

    Make new data

--- src/server/Services/DataService.lua
+++ src/server/Services/DataService.lua
@@ -10,7 +10,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerModules = script.Parent.Parent.Modules
 local ProfileService = require(ServerModules.ProfileService)
 local ProfileTemplate = require(script.Parent.Parent.Constants.DataTemplate)
-local ProfileStore = ProfileService.GetProfileStore("DataTest_1_15", ProfileTemplate)
+local ProfileStore = ProfileService.GetProfileStore("DataTest_1_16", ProfileTemplate)
 
 local BiomesConfig = require(ReplicatedStorage.Shared.Data.Shop.Biomes)
 
